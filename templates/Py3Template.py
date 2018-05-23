402df23e259a6820308e64b971368d439a62dfca
Keith Barr <keith.barr@aerosys-eng.com>
2018-05-22 18:02:13 -0600

    First commit

---
+++ templates/Py3Template.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+************************************************************************************
+Copyright 2018 (C) AeroSys Engineering, Inc.
+All Rights Reserved
+
+This source code is provided with no warranty expressed or implied.  Suitability for
+any purpose is not guaranteed.
+************************************************************************************
+
+Revision History:
+
+2018-05-20, ksb, created
+"""
+
+import sys
+sys.path.append("..")
+
+# define a version for this file
+VERSION = "1.0.20180520a"
+
+if __name__ == '__main__':
+    # when this file is run directly, run this code
+    print(VERSION)
