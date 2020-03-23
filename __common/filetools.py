5ba16c96188b4d6b9f9e1a5e5b59a930e8b3d07d
Keith Barr <keith.barr@aerosys-eng.com>
2020-03-23 12:05:39 -0600

    Added scp capability to system

--- __common/filetools.py
+++ __common/filetools.py
@@ -16,10 +16,13 @@ Revision History:
 
 from pathlib import Path
 import shutil
+import os
+import re
+
 
 
 # define a version for this file
-VERSION = "1.0.2018-05-25a"
+VERSION = "1.0.20200323a"
 
 def mkdir(pathname):
   """If a given pathname doesn't exist, build it
@@ -63,8 +66,93 @@ def cp(src, dst):
     return False
   return True
 
+def get_listing(pathname, regexp):
+  """Get a listing of the files in a path matching regexp
+  
+  pathname: directory to search
+  regexp: regular expression string"""
+  print("filetools.get_listing(): Searching {:s} for files matching {:s}".format(pathname, regexp))
+  
+  # work through everything in the directory
+  listing = []
+  for filename in os.listdir(pathname):
+    # is this a file?
+    if os.path.isfile(pathname + "/" + filename):
+      # does it match our expression?
+      if re.search(regexp, filename):
+        listing.append(pathname + "/" + filename)
+  
+  # sort our listing and return
+  listing.sort()
+  return listing
 
+def rm(filename):
+  """Remove a file
+  
+  filename: full pathname of the file to remove"""
+  try:
+    os.remove(filename)
+  except Exception:
+    print("filetools.rm():  Unable to remove file {:s}".format(filename))
+    return False
+  
+  return True
+
+def wait_for_change(path_to_watch):
+  """block waiting for a directory to change
+  
+  path_to_watch: the directory to wait for changes in"""
+  import win32file
+  import win32con
+  
+  ACTIONS = {
+    1 : "Created",
+    2 : "Deleted",
+    3 : "Updated",
+    4 : "Renamed from something",
+    5 : "Renamed to something"
+  }
+  # Thanks to Claudio Grondi for the correct set of numbers
+  FILE_LIST_DIRECTORY = 0x0001
+  
+  hDir = win32file.CreateFile (
+    path_to_watch,
+    FILE_LIST_DIRECTORY,
+    win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE | win32con.FILE_SHARE_DELETE,
+    None,
+    win32con.OPEN_EXISTING,
+    win32con.FILE_FLAG_BACKUP_SEMANTICS,
+    None
+  )
+  
+  #
+  # ReadDirectoryChangesW takes a previously-created
+  # handle to a directory, a buffer size for results,
+  # a flag to indicate whether to watch subtrees and
+  # a filter of what changes to notify.
+  #
+  # NB Tim Juchcinski reports that he needed to up
+  # the buffer size to be sure of picking up all
+  # events when a large number of files were
+  # deleted at once.
+  #
+  results = win32file.ReadDirectoryChangesW (hDir,
+                                             1024,
+                                             True,
+                                             win32con.FILE_NOTIFY_CHANGE_FILE_NAME |
+                                             win32con.FILE_NOTIFY_CHANGE_DIR_NAME |
+                                             win32con.FILE_NOTIFY_CHANGE_ATTRIBUTES |
+                                             win32con.FILE_NOTIFY_CHANGE_SIZE |
+                                             win32con.FILE_NOTIFY_CHANGE_LAST_WRITE |
+                                             win32con.FILE_NOTIFY_CHANGE_SECURITY,
+                                             None,
+                                             None)
+  
+  return results
 
 if __name__ == '__main__':
   # when this file is run directly, run this code
   print(VERSION)
+  
+  path_to_watch = r"C:\Temp\Wx_Raw"
+  results = wait_for_change(path_to_watch)
---
+++ weather_station/sftp_upload.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+"""
+************************************************************************************
+Copyright 2020 (C) AeroSys Engineering, Inc.
+All Rights Reserved
+
+This source code is provided with no warranty expressed or implied.  Suitability for
+any purpose is not guaranteed.
+************************************************************************************
+
+Revision History:
+
+2020-03-23, ksb, created
+"""
+
+import sys
+sys.path.append("..")
+import __common.filetools as filetools
+
+import subprocess
+import time
+
+# define a version for this file
+VERSION = "1.0.20200323a"
+
+raw_dir = r"C:\Temp\Wx_Raw"
+image_file = r"\\roofpi\share\timelapse\image.jpg"
+
+if __name__ == '__main__':
+  print(VERSION)
+  
+  while True:
+    # wait for changes to be detected in the directory
+    filetools.wait_for_change(raw_dir)
+    
+    # wait for the files to complete
+    time.sleep(5)
+    
+    # get the latest webcam image
+    filetools.cp(image_file, raw_dir)
+    
+    # get a listing of the files
+    listing = filetools.get_listing(raw_dir, r"\.jpg")
+    
+    # scp the files to the server
+    subprocess.run(["C:\Program Files\PuTTY\pscp.exe",
+                    "-P", "4830",
+                    "C:\Temp\Wx_Raw\*.jpg", 
+                    "keithbarr@droplet1.colorado-barrs.com:/home/keithbarr/public_html/wx"])
+    
+    # delete each file
+    for file in listing:
+      # now delete the file because we are done with it
+      filetools.rm(file)
+
+  
+  
+      
